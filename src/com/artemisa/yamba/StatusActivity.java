041fa8e2806df82b2e5eab610acd656413f22d7f
Juan García <juangarcia@mailinator.com>
2013-03-26 15:44:49 +0000

    Other UI Events

--- src/com/artemisa/yamba/StatusActivity.java
+++ src/com/artemisa/yamba/StatusActivity.java
@@ -3,22 +3,28 @@ package com.artemisa.yamba;
 import winterwell.jtwitter.Twitter;
 import winterwell.jtwitter.TwitterException;
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class StatusActivity extends Activity implements OnClickListener {
+public class StatusActivity extends Activity implements OnClickListener,
+		TextWatcher {
 	private static final String TAG = "StatusActivity";
 	private Button updateButton;
-	private TextView editText;
+	private EditText editText;
 	private Twitter twitter;
-
+	private TextView textCount;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -28,7 +34,12 @@ public class StatusActivity extends Activity implements OnClickListener {
 		updateButton = (Button) findViewById(R.id.buttonUpdate);
 		updateButton.setOnClickListener(this);
 
-		editText = (TextView) findViewById(R.id.editText);
+		editText = (EditText) findViewById(R.id.editText);
+		editText.addTextChangedListener(this);
+
+		textCount = (TextView) findViewById(R.id.textCount);
+		textCount.setText(Integer.toString(140));
+		textCount.setTextColor(Color.GREEN);
 
 		twitter = new Twitter("student", "password");
 		twitter.setAPIRootUrl("http://yamba.marakana.com/api");
@@ -69,6 +80,7 @@ public class StatusActivity extends Activity implements OnClickListener {
 			Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT)
 					.show();
 		}
+
 	}
 
 	// Called when button is clicked
@@ -77,4 +89,35 @@ public class StatusActivity extends Activity implements OnClickListener {
 		new PostToTwitter().execute(status);
 	}
 
+	// Called when editText's text changes
+	@Override
+	public void afterTextChanged(Editable s) {
+		Log.d(TAG, "afterTextChanged()");
+
+		int count = 140 - s.length();
+		String text = Integer.toString(count);
+
+		textCount.setText(text);
+
+		if (count > 10) {
+			textCount.setTextColor(Color.GREEN);
+		} else {
+			if (count >= 1 && count <= 10) {
+				textCount.setTextColor(Color.YELLOW);
+			} else {
+				textCount.setTextColor(Color.RED);
+			}
+		}
+	}
+
+	@Override
+	public void beforeTextChanged(CharSequence s, int start, int count,
+			int after) {
+		// not used
+	}
+
+	@Override
+	public void onTextChanged(CharSequence s, int start, int before, int count) {
+		// no used
+	}
 }
