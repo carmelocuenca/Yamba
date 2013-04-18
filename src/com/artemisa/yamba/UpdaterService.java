2d1a02a23ca4742100196071b27ec24feb037792
Juan García <juangarcia@mailinator.com>
2013-04-18 19:26:28 +0100

    Looping in the Service

--- src/com/artemisa/yamba/UpdaterService.java
+++ src/com/artemisa/yamba/UpdaterService.java
@@ -6,7 +6,10 @@ import android.os.IBinder;
 import android.util.Log;
 
 public class UpdaterService extends Service {
-	static final String TAG = "UpdaterService";
+	private static final String TAG = "UpdaterService";
+	private static final int DELAY = 60000; // a minute
+	private boolean runFlag = false;
+	private Updater updater;
 
 	@Override
 	public IBinder onBind(Intent arg0) {
@@ -18,13 +21,21 @@ public class UpdaterService extends Service {
 	public void onCreate() {
 		// TODO Auto-generated method stub
 		super.onCreate();
+
+		updater = new Updater();
+
 		Log.d(TAG, "onCreated");
+
 	}
 
 	@Override
 	public void onDestroy() {
 		// TODO Auto-generated method stub
 		super.onDestroy();
+
+		runFlag = false;
+		updater.interrupt();
+		updater = null;
 		Log.d(TAG, "onDestroyed");
 	}
 
@@ -32,8 +43,39 @@ public class UpdaterService extends Service {
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		// TODO Auto-generated method stub
 		super.onStartCommand(intent, flags, startId);
+
+		runFlag = true;
+		updater.start();
+
 		Log.d(TAG, "onStarted");
 		return START_STICKY;
+	}
+
+	/*
+	 * Threads tha permos the actual update from the online service
+	 */
+	private class Updater extends Thread {
+
+		private Updater() {
+			super("UpdaterService-Updater");
+			// TODO Auto-generated constructor stub
+		}
+
+		@Override
+		public void run() {
+			UpdaterService updaterService = UpdaterService.this;
+			while (updaterService.runFlag) {
+				Log.d(TAG, "Updater running");
+				try {
+					// Some work goes here ...
+					Log.d(TAG, "Updater ran");
+					Thread.sleep(DELAY);
+				} catch (InterruptedException e) {
+					updaterService.runFlag = false;
+				}
+			}
+		}
+
 	}
 
 }
